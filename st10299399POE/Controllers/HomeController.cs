ba0335d52d45b73c6b5ac52e66fee6e61cd4c5cc
Ethan Scott Parsons <ST10299399@vcconnect.edu.za>
2024-03-22 13:22:30 +0200

    Add project files.

---
+++ st10299399POE/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
+using st10299399POE.Models;
+using System.Diagnostics;
+
+namespace st10299399POE.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult MyWork()
+        {
+            return View();
+        }
+
+        public IActionResult AboutUS()
+        {
+            return View();
+        }
+
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ st10299399POE/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace st10299399POE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
